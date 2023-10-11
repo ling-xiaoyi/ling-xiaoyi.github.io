d6575d45b415c379d854db20cb0eec4647b65fb2
ling-xiaoyi <2080204533@qq.com>
2023-10-11 16:15:31 +0800

    Site updated: 2023-10-11 16:15:31

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["2023/10/10/hello-world/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["2023/10/10/hello-world/","2023/10/11/title/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
