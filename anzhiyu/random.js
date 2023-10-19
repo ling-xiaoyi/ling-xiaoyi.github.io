b63f702c0f3ef88bb46d99d3f0a15510c74bdd43
ling-xiaoyi <2080204533@qq.com>
2023-10-19 15:43:16 +0800

    Site updated: 2023-10-19 15:43:15

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["2023/10/13/appjks/","2023/10/10/hello-world/","2023/10/11/android/","2023/10/11/title/","2023/10/12/前端基础JS/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["2023/10/11/android/","2023/10/13/appjks/","2023/10/10/hello-world/","2023/10/11/title/","2023/10/12/前端基础JS/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
